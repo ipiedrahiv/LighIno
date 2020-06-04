5c4891abb94f665502d1f809910874d37874fb1f
Isabel Piedrahita <ipiedrahiv@eafit.edu.co>
2020-06-03 21:01:39 -0500

    Documented .ino code

--- ProyectoFinal/ProyectoFinal.ino
+++ ProyectoFinal/ProyectoFinal.ino
@@ -1,39 +1,18 @@
-/*
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
-*/
 int data;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+  // initialize digital pin 8 as an output.
   Serial.begin(9600);
   pinMode(8, OUTPUT);
   digitalWrite (8, 0);
-  Serial.println("Hello!,How are you Python ?");
+  // initial message for python/arduino coordination
+  Serial.println("Hello! How are you Python?");
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  // Reads the information written by python
   while(Serial.available()){
     data = Serial.read();
   }
@@ -41,9 +20,9 @@ void loop() {
   Serial.println(data);
 
   if (data == '1'){
-    digitalWrite (8, 1); 
+    digitalWrite (8, 1);		//Turns ON the led
   }else if (data == '0'){
-    digitalWrite (8, 0);                  //Turn OFF the Led
+    digitalWrite (8, 0);                //Turn OFF the Led
   }
 
 }
